7a3dbdcb0cfd0f7085b1aa4ef39fb6ac10f5685a
John Hurst <john.b.hurst@gmail.com>
2021-12-10 09:31:22 +1300

    feat: Improved IO using Data.Text.

--- advent1.hs
+++ advent1.hs
@@ -1,18 +1,15 @@
 import System.Environment
-import System.IO
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 countLarger :: [Integer] -> Integer
 countLarger (h1:h2:t) = (if h2 > h1 then 1 else 0) + countLarger (h2:t)
 countLarger l = 0
 
 main = do
-    args <- getArgs
-    let fileName = head args
-    handle <- openFile fileName ReadMode
-    contents <- hGetContents handle
-    let singleWords = words contents
-        ints = map (read::String->Integer) singleWords
+    fileName <- fmap head getArgs
+    lines <- fmap Text.lines (Text.readFile fileName)
+    let ints = map toInt lines
+            where toInt = read . Text.unpack
         result = countLarger ints
     print result
-    hClose handle
-
--- advent2.hs
+++ advent2.hs
@@ -1,18 +1,15 @@
 import System.Environment
-import System.IO
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
 
 countLargerWindow :: [Integer] -> Integer
-countLargerWindow (h1:h2:h3:h4:t) = (if h2 + h3 + h4 > h1 + h2 + h3 then 1 else 0) + countLargerWindow (h2:h3:h4:t)
+countLargerWindow (h1:h2:h3:h4:t) = (if h2+h3+h4 > h1+h2+h3 then 1 else 0) + countLargerWindow (h2:h3:h4:t)
 countLargerWindow l = 0
 
 main = do
-    args <- getArgs
-    let fileName = head args
-    handle <- openFile fileName ReadMode
-    contents <- hGetContents handle
-    let singleWords = words contents
-        ints = map (read::String->Integer) singleWords
+    fileName <- fmap head getArgs
+    lines <- fmap Text.lines (Text.readFile fileName)
+    let ints = map toInt lines
+            where toInt = read . Text.unpack
         result = countLargerWindow ints
     print result
-    hClose handle
-
