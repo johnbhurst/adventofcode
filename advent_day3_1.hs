3df027ce4569b3b743bdfd5e9ef891baec17f2d4
John Hurst <john.b.hurst@gmail.com>
2021-12-13 06:47:37 +1300

    refactor: Changed from '0's and '1's to Bools.

--- advent_day3_1.hs
+++ advent_day3_1.hs
@@ -12,20 +12,23 @@ import qualified Data.Text.Lazy as Text
 import qualified Data.Text.Lazy.IO as Text
 import System.Environment
 
-type BinaryDigits = [Text.Text]
+type BinaryDigits = [Bool]
 type BinaryDigitsList = [BinaryDigits]
 type BinaryDigitFrequency = (Int, Int)
 type BinaryDigitFrequencies = [BinaryDigitFrequency]
 
--- ["010", "110", "001"] -> [["0", "1", "0"], ["1", "1", "0"], ["0", "0", "1"]]
+-- ["010", "110", "001"] -> [[False, True, False], [True, True, False], [False, False, True]]
 makeBinaryDigits :: [Text.Text] -> [BinaryDigits]
-makeBinaryDigits = map $ Text.chunksOf 1
+makeBinaryDigits = map (toBools . toChars)
+    where toChars = Text.chunksOf 1
+          toBools = map toBool
+          toBool c = c == Text.pack "1"
 
--- [["0", "1", "0"], ["1", "1", "0"], ["0", "0", "1"]] -> [[(1,0), (0,1), (1,0)], [(0,1), (0,1), (1,0)], [(1,0), (1,0), (0,1)]]
+-- [[False, True, False], [True, True, False], [False, False, True]] -> [[(1,0), (0,1), (1,0)], [(0,1), (0,1), (1,0)], [(1,0), (1,0), (0,1)]]
 makeDigitFrequencies :: [BinaryDigits] -> [BinaryDigitFrequencies]
 makeDigitFrequencies = map freqRow
     where freqRow = map freqDigit
-          freqDigit c = if c == Text.pack "0" then (1,0) else (0,1)  
+          freqDigit b = if b then (0,1) else (1,0)
 
 -- [[(1,0), (0,1), (1,0)], [(0,1), (0,1), (1,0)], [(1,0), (1,0), (0,1)]] -> [(2,1), (1,2), (2,1)]
 accumulateDigitFrequencies :: [BinaryDigitFrequencies] -> BinaryDigitFrequencies
@@ -33,20 +36,19 @@ accumulateDigitFrequencies = foldl1 accum
     where accum = zipWith combine
           combine (z1, o1) (z2, o2) = (z1+z2, o1+o2)
 
--- [(2,1), (1,2), (2,1)] -> ["1", "0", "1"]
+-- [(2,1), (1,2), (2,1)] -> [True, False, True]
 selectDigits :: BinaryDigitFrequencies -> BinaryDigits
 selectDigits = map selectDigit
-    where selectDigit (zeros, ones) = if zeros > ones then Text.pack "0" else Text.pack "1"
+    where selectDigit (zeros, ones) = ones >= zeros
 
--- ["1", "0", "1"] -> 1x4 + 0x2 + 1x1 = 5
+-- [True, False, True] -> 1x4 + 0x2 + 1x1 = 5
 toNumber :: BinaryDigits -> Int
 toNumber = foldl accum 0
-    where accum result digit = 2 * result + if digit == Text.pack "0" then 0 else 1
+    where accum result bit = 2 * result + if bit then 1 else 0
 
--- ["1", "0", "1"] -> ["0", "1", "0"]
+-- [True, False, True] -> [False, True, False]
 complement :: BinaryDigits -> BinaryDigits
-complement = map inverse 
-    where inverse c = if c == Text.pack "0" then Text.pack "1" else Text.pack "0"
+complement = map not
 
 main = do
     [fileName] <- getArgs
