04cb2bfd9a45c6cfa8fdf01162c705916eb40509
John Hurst <john.b.hurst@gmail.com>
2022-12-01 10:01:36 +1300

    feat: Added f# version of day1_2.

---
+++ 2021/fsharp/day1_2/day1_2.fs
@@ -0,0 +1,12 @@
+// Try advent 2021 day 1 in F#
+
+let rec countLargerWindow l =
+    match l with
+    | h1::h2::h3::h4::t -> (if h2+h3+h4 > h1+h2+h3 then 1 else 0) + countLargerWindow (h2::h3::h4::t)
+    | _ -> 0
+
+let result = System.IO.File.ReadLines( System.Environment.GetCommandLineArgs().[1] )
+                |> List.ofSeq
+                |> List.map int
+                |> countLargerWindow
+printfn "%d" result
