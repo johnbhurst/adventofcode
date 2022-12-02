c5dfd7e92d234c328d44cfdc1886e2845b5d8291
John Hurst <john.b.hurst@gmail.com>
2022-12-03 11:24:20 +1300

    feat: Solution for day2/2.

---
+++ 2022/day2_2/day2_2.fs
@@ -0,0 +1,23 @@
+// Copyright 2022 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2022-12-03
+
+let score line =
+    match line with
+    | "A X" -> 3 + 0
+    | "A Y" -> 1 + 3
+    | "A Z" -> 2 + 6
+    | "B X" -> 1 + 0
+    | "B Y" -> 2 + 3
+    | "B Z" -> 3 + 6
+    | "C X" -> 2 + 0
+    | "C Y" -> 3 + 3
+    | "C Z" -> 1 + 6
+    | _ -> 0
+
+let result = System.IO.File.ReadLines( System.Environment.GetCommandLineArgs().[1] )
+                |> List.ofSeq
+                |> List.map score
+                |> List.sum
+
+printf "%A\n" result
