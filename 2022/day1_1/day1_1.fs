906dc86a5751ce372391eda599963cb1f9afcc0c
John Hurst <john.b.hurst@gmail.com>
2022-12-03 10:57:17 +1300

    feat: Solution for day1/1.

---
+++ 2022/day1_1/day1_1.fs
@@ -0,0 +1,19 @@
+// Copyright 2022 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2022-12-01
+
+let rec splitlist sep xs =
+    match xs with
+    | [] -> []
+    | h :: t -> if (h = sep) then [] :: splitlist sep t else
+                    match splitlist sep t with
+                    | [] -> [[h]]
+                    | h2 :: t2 -> (h :: h2) :: t2
+
+let result = System.IO.File.ReadLines( System.Environment.GetCommandLineArgs().[1] )
+                |> List.ofSeq
+                |> splitlist ""
+                |> List.map (fun xs -> xs |> List.map int |> List.sum)
+                |> List.max
+
+printf "%A\n" result
