c6a0eafa9452b7ddb4fa56d41e8c71e922807395
John Hurst <john.b.hurst@gmail.com>
2022-12-04 09:10:45 +1100

    feat: Solutions for day3.

---
+++ 2022/day3/day3_1.fsx
@@ -0,0 +1,23 @@
+// Copyright 2022 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2022-12-04
+
+let split (s:string) = s.Substring(0, s.Length / 2), s.Substring(s.Length / 2)
+
+let finddup (s1, s2) =
+    let l1 = Seq.toList s1
+    let l2 = Seq.toList s2
+    List.find (fun c -> List.exists (fun c2 -> c2=c) l2) l1
+
+let score c =
+    match c with
+    | t when t >= 'a' && t <= 'z' -> 1 + (int t - int 'a')
+    | t when t >= 'A' && t <= 'Z' -> 27 + (int t - int 'A')
+    | _ -> 0
+
+let result = System.IO.File.ReadLines( fsi.CommandLineArgs.[1] )
+                |> List.ofSeq
+                |> List.map (split >> finddup >> score)
+                |> List.sum
+
+printf "%A\n" result
---
+++ 2022/day3/day3_2.fsx
@@ -0,0 +1,30 @@
+// Copyright 2022 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2022-12-04
+
+let rec partition l =
+    match l with
+    | h1 :: h2 :: h3 :: t -> (h1, h2, h3) :: partition t
+    | _ -> []
+
+let finddup (s1, s2, s3) =
+    let l1 = Seq.toList s1
+    let l2 = Seq.toList s2
+    let l3 = Seq.toList s3
+    let contains c l = List.exists (fun c2 -> c2=c) l
+    List.find (fun c -> contains c l2 && contains c l3) l1
+    // List.find (fun c -> List.exists (fun c2 -> c2=c) l2 && List.exists (fun c2 )) l1
+
+let score c =
+    match c with
+    | t when t >= 'a' && t <= 'z' -> 1 + (int t - int 'a')
+    | t when t >= 'A' && t <= 'Z' -> 27 + (int t - int 'A')
+    | _ -> 0
+
+let result = System.IO.File.ReadLines( fsi.CommandLineArgs.[1] )
+                |> List.ofSeq
+                |> partition
+                |> List.map (finddup >> score)
+                |> List.sum
+
+printf "%A\n" result
