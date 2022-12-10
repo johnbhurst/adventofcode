ad0b2f6ad9a77410112ccc5cd0033d3880b79de1
John Hurst <john.b.hurst@gmail.com>
2022-12-10 14:57:47 +1100

    refactor: Rearranged, tidied up code.

--- 2022/day5/day5_1.fsx
+++ 2022/day5/day5_1.fsx
@@ -5,11 +5,15 @@
 
 open System.Text.RegularExpressions
 
+type StackSet = { n: int; items: char list[] }
+
+// split a string into a list of 3-character strings
 let rec splitTo3 str =
     match str with
     | "" -> []
     | _ -> str.[0..2] :: splitTo3 str.[4..]
 
+// split a list of strings into a pair of lists, where the first list ends with an empty string
 let rec split input =
     match input with
     | [] -> ([], [])
@@ -20,23 +24,21 @@ let rec split input =
             let (first, rest) = split rest
             (line :: first, rest)
 
-type StackSet = { n: int; items: char list[] }
-
-// function to create a StackSet of size n
+// create a StackSet of size n
 let create n =
     { n = n; items = Array.init n (fun _ -> []) }
 
-// function to push item to StackSet
-let pushItem x item stackSet =
-    stackSet.items.[x] <- item :: stackSet.items.[x]
+// push item to ith stack of StackSet
+let pushItem i item stackSet =
+    stackSet.items.[i] <- item :: stackSet.items.[i]
     stackSet
 
-// function to pop an item from StackSet
-let popItem x stackSet =
-    match stackSet.items.[x] with
+// pop an item from ith stack of StackSet
+let popItem i stackSet =
+    match stackSet.items.[i] with
     | [] -> failwith "popItem: empty stack"
     | item :: rest ->
-        stackSet.items.[x] <- rest
+        stackSet.items.[i] <- rest
         item
 
 // let rec move n src dst stackSet =
@@ -49,9 +51,44 @@ let move n src dst stackSet =
         pushItem dst (popItem src stackSet) stackSet |> ignore
     stackSet
 
-let createCommand str =
-    create 3
+let createRegex = Regex(@"^( +\d)+$")
+let parseCreate str =
+    let m = createRegex.Match(str)
+    if m.Success then
+        let n = int m.Groups.[1].Value
+        create n
+    else
+        failwith "parseCreate: invalid create"
 
+let setupRegex = Regex(@"^\[([A-Z])\]$")
+let (|Setup|_|) str =
+    let m = setupRegex.Match(str)
+    if m.Success then Some(m.Groups.[1].Value.[0])
+    else None
+let (|Nop|_|) str =
+    match str with
+    | "   " -> Some()
+    | _ -> None
+let parse1Setup i x =
+    match x with
+    | Setup x -> Some(pushItem i x)
+    | Nop _ -> None
+    | _ -> failwith "parse1Setup: invalid setup"
+let parseSetup str =
+    splitTo3 str |> List.mapi parse1Setup
+
+let moveRegex = Regex(@"^move (\d+) from (\d+) to (\d+)$")
+let parseMove str =
+    let m = moveRegex.Match(str)
+    if m.Success then
+        let n = int m.Groups.[1].Value
+        let src = int m.Groups.[2].Value
+        let dst = int m.Groups.[3].Value
+        move n (src-1) (dst-1)
+    else
+        failwith "parseMove: invalid move"
+
+// MAIN PROGRAM
 let lines = System.IO.File.ReadLines( fsi.CommandLineArgs.[1] )
                 |> List.ofSeq
 
@@ -66,58 +103,14 @@ let (createLine, setupLines) =
 // printfn "createLine=[%A]" createLine
 // printfn "setupLines=[%A]" setupLines
 
-let moveRegex = Regex(@"^move (\d+) from (\d+) to (\d+)$")
-
-let parseMove str =
-    let m = moveRegex.Match(str)
-    if m.Success then
-        let n = int m.Groups.[1].Value
-        let src = int m.Groups.[2].Value
-        let dst = int m.Groups.[3].Value
-        move n (src-1) (dst-1)
-    else
-        failwith "parseMove: invalid move"
-
-let createRegex = Regex(@"^( +\d)+$")
-
-let parseCreate str =
-    let m = createRegex.Match(str)
-    if m.Success then
-        let n = int m.Groups.[1].Value
-        create n
-    else
-        failwith "parseCreate: invalid create"
-
-let setupRegex = Regex(@"^\[([A-Z])\]$")
-
-let (|Setup|_|) str =
-    let m = setupRegex.Match(str)
-    if m.Success then Some(m.Groups.[1].Value.[0])
-    else None
-
-let (|Nop|_|) str =
-    match str with
-    | "   " -> Some()
-    | _ -> None
-
-let parse1Setup i x =
-    match x with
-    | Setup x -> Some(pushItem i x)
-    | Nop _ -> None
-    | _ -> failwith "parse1Setup: invalid setup"
-
-let parseSetup str =
-    splitTo3 str |> List.mapi parse1Setup
-
 let initialStacks = parseCreate createLine
 let setups = setupLines |> List.map parseSetup |> List.concat |> List.choose id
 
-let stacks = List.fold (fun s setup -> setup s) initialStacks setups
+let stacks = setups |> List.fold (fun stack setup -> setup stack) initialStacks
 // printfn "stacks=[%A]" stacks
 
 let moves = moveLines |> List.map parseMove
-let finalStacks = moves |> List.fold (fun s f -> f s) stacks
-
+let finalStacks = moves |> List.fold (fun stack move -> move stack) stacks
 // printfn "finalStacks=[%A]" finalStacks
 
 let result = finalStacks.items |> Array.map List.head |> Array.map string |> String.concat ""
