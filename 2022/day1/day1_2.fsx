3f72013e0111551b01e4e79933e0dffc3cc995b9
John Hurst <john.b.hurst@gmail.com>
2022-12-21 01:14:57 +1300

    refactor: rewrote day1_2 more elegantly.

--- 2022/day1/day1_2.fsx
+++ 2022/day1/day1_2.fsx
@@ -2,21 +2,22 @@
 // Copyright 2022 John Hurst
 // John Hurst (john.b.hurst@gmail.com)
 // 2022-12-01
-//See https://adventofcode.com/2022/day/1
+// See https://adventofcode.com/2022/day/1
 
-let rec splitlist sep xs =
-    match xs with
-    | [] -> []
-    | h :: t -> if (h = sep) then [] :: splitlist sep t else
-                    match splitlist sep t with
-                    | [] -> [[h]]
-                    | h2 :: t2 -> (h :: h2) :: t2
+let split (sep:string) (str:string) = str.Split(sep)
+
+// group lines into lists of lines separated by blank lines
+let groupLines lines =
+    lines
+    |> String.concat "#"
+    |> split "##"
+    |> Seq.map (split "#")
 
 System.IO.File.ReadLines( fsi.CommandLineArgs.[1] )
-    |> List.ofSeq
-    |> splitlist ""
-    |> List.map (fun xs -> xs |> List.map int |> List.sum)
-    |> List.sortBy (fun x -> -x)
-    |> List.take 3
-    |> List.sum
+    |> groupLines
+    |> Seq.map (Seq.map int)
+    |> Seq.map Seq.sum
+    |> Seq.sortDescending
+    |> Seq.take 3
+    |> Seq.sum
     |> printfn "%A"
