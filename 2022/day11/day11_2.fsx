a8acb2ff08dfd1db5f0ddd2989ea027f44c10d17
John Hurst <john.b.hurst@gmail.com>
2022-12-25 09:29:41 +1300

    feat: Much simplified parsing after reading Malcolm's solution.

--- 2022/day11/day11_2.fsx
+++ 2022/day11/day11_2.fsx
@@ -8,76 +8,40 @@
 // In this implementation, the worry levels grow ridiculously large, and cannot be computed to full precision in a reasonable time.
 // Instead, we record the worry level as a set of remainders modulo the test divisors. This will be sufficient for evaluating the test condition for each monkey.
 
+open System.Text.RegularExpressions
+
 type Monkey = { Monkey: int; StartingItems: int list; Operation: int -> int -> int; TestDivisible: int; IfTrue: int; IfFalse: int }
 
 let (|Add|_|) (str:string) = if str.StartsWith("new = old + ") then Some(str.Substring(12) |> int) else None
 let (|Multiply|_|) (str:string) = if str.StartsWith("new = old * ") then Some(str.Substring(12) |> int) else None
 let (|Square|_|) (str:string) = if str = "new = old * old" then Some() else None
 
-let rec parseMonkeys (lines: string list) =
-    match lines with
+let matchRegex regexStr str =
+    let m = Regex(regexStr).Match(str)
+    if m.Success then m.Groups.[1].Value else failwith ("Expected " + regexStr + ", got line " + str)
+
+let rec parse lines =
+    match (lines: string list) with
+    | monkeyLine :: startingItemsLine :: operationLine :: testLine :: ifTrueLine :: ifFalseLine :: rest ->
+
+        let monkey = matchRegex "Monkey ([0-9]+):" monkeyLine |> int
+        let startingItems = matchRegex "  Starting items: ([0-9]+(, [0-9]+)*)" startingItemsLine |> (fun s -> s.Split(",")) |> Seq.map int |> List.ofSeq
+        let operation = match matchRegex "  Operation: (.*)" operationLine with
+                        | Square _ -> fun divisor worry -> (worry * worry) % divisor
+                        | Multiply n -> fun divisor worry -> (worry * n) % divisor
+                        | Add n -> fun divisor worry -> (worry + n) % divisor
+                        | _ -> failwith ("Unknown operation" + operationLine)
+        let testDivisible = matchRegex "  Test: divisible by ([0-9]+)" testLine |> int
+        let ifTrue = matchRegex "    If true: throw to monkey ([0-9]+)" ifTrueLine |> int
+        let ifFalse = matchRegex "    If false: throw to monkey ([0-9]+)" ifFalseLine |> int
+        { Monkey = monkey; StartingItems = startingItems; Operation = operation; TestDivisible = testDivisible; IfTrue = ifTrue; IfFalse = ifFalse } :: parse rest
     | [] -> []
-    | line :: rest ->
-        if line.StartsWith("Monkey ") then
-            let s = line.Substring(7)
-            let monkey = s.[0..s.Length - 2] |> int
-            parseStartingItems rest monkey
-        else
-            failwith ("Expected 'Monkey: ', got line " + line)
-and parseStartingItems lines monkey =
-    match lines with
-    | line :: rest ->
-        if line.StartsWith("  Starting items: ") then
-            let startingItems = line.Substring(18).Split(", ") |> Seq.map int |> List.ofSeq
-            parseOperation rest monkey startingItems
-        else
-            failwith ("Expected 'Starting items: ', got line " + line)
-    | _ -> failwith "Expected 'Starting items: '"
-and parseOperation lines monkey startingItems =
-    match lines with
-    | line :: rest ->
-        if line.StartsWith("  Operation: ") then
-            let operation = match (line.Substring(13)) with
-                            | Square _ -> fun divisor worry -> (worry * worry) % divisor
-                            | Multiply n -> fun divisor worry -> (worry * n) % divisor
-                            | Add n -> fun divisor worry -> (worry + n) % divisor
-                            | _ -> failwith "Unknown operation"
-            parseTestDivisible rest monkey startingItems operation
-        else
-            failwith ("Expected 'Operation: ', got line " + line)
-    | _ -> failwith "Expected 'Operation: '"
-and parseTestDivisible lines monkey startingItems operation =
-    match lines with
-    | line :: rest ->
-        if line.StartsWith("  Test: divisible by ") then
-            let testDivisible = line.Substring(21) |> int
-            parseIfTrue rest monkey startingItems operation testDivisible
-        else
-            failwith ("Expected 'Test divisible: ', got line " + line)
-    | _ -> failwith "Expected 'Test divisible: '"
-and parseIfTrue lines monkey startingItems operation testDivisible =
-    match lines with
-    | line :: rest ->
-        if line.StartsWith("    If true: throw to monkey ") then
-            let ifTrue = line.Substring(29) |> int
-            parseIfFalse rest monkey startingItems operation testDivisible ifTrue
-        else
-            failwith ("Expected 'If true: ', got line " + line)
-    | _ -> failwith "Expected 'If true: '"
-and parseIfFalse lines monkey startingItems operation testDivisible ifTrue =
-    match lines with
-    | line :: rest ->
-        if line.StartsWith("    If false: throw to monkey ") then
-            let ifFalse = line.Substring(30) |> int
-            { Monkey = monkey; StartingItems = startingItems; Operation = operation; TestDivisible = testDivisible; IfTrue = ifTrue; IfFalse = ifFalse } :: parseMonkeys rest
-        else
-            failwith ("Expected 'If false: ', got line " + line)
-    | _ -> failwith "Expected 'If false: '"
+    | _ -> failwith "Unexpected number of lines"
 
 let monkeys = System.IO.File.ReadLines( fsi.CommandLineArgs.[1] )
                 |> Seq.toList
-                |> List.filter (fun x -> x <> "")
-                |> parseMonkeys
+                |> List.filter (fun line -> line <> "")
+                |> parse
 
 // for a given starting item, make the map of all the monkey divisors to the remainder of the starting item modulo that divisor
 let makeRemainderMap worry =
