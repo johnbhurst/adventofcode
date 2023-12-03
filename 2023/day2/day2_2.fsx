8eb5fa7e65b6fa92fab2a71551f368933f8d2d32
John Hurst <john.b.hurst@gmail.com>
2023-12-03 13:14:11 +1300

    refactor: Added sample input; slightly simplified parsing code.

--- 2023/day2/day2_2.fsx
+++ 2023/day2/day2_2.fsx
@@ -4,6 +4,13 @@
 // 2023-12-03
 // See https://adventofcode.com/2023/day/2
 
+// Sample input:
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
 open System.Text.RegularExpressions
 
 type Combo = { Red: int; Green: int; Blue: int}
@@ -20,9 +27,9 @@ let parseColor s =
 
 let parseCombo (s:string) =
     let m = (s.Split(", ")) |> Seq.map parseColor  |> Map.ofSeq
-    { Red = (if Map.containsKey "red" m then Map.find "red" m else 0);
-        Green = (if Map.containsKey "green" m then Map.find "green" m else 0);
-        Blue = (if Map.containsKey "blue" m then Map.find "blue" m else 0) }
+    { Red = Map.tryFind "red" m |> Option.defaultValue 0;
+        Green = Map.tryFind "green" m |> Option.defaultValue 0;
+        Blue = Map.tryFind "blue" m |> Option.defaultValue 0 }
 
 let parseCombos (s:string) =
     Seq.map parseCombo (s.Split("; "))
