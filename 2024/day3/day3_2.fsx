9b5d4c9169079b8d5db4e7f637037aa3323eed77
John Hurst <john.b.hurst@gmail.com>
2024-12-03 18:59:23 +1100

    feat: Added 2024 day3_2 solution.

---
+++ 2024/day3/day3_2.fsx
@@ -0,0 +1,34 @@
+#!/usr/bin/env -S dotnet fsi
+// Copyright 2024 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2024-12-03
+// See https://adventofcode.com/2024/day/3
+
+open System
+open System.Text.RegularExpressions
+
+let rec parseMul (s:string) =
+    let m = Regex.Match(s, @"(don't\(\))(.*)|mul\(([0-9]{1,3}),([0-9]{1,3})\)(.*)")
+    if m.Success then
+        if m.Groups.[1].Value = "don't()" then
+            parseNoMul m.Groups.[2].Value
+        else
+            let a = int m.Groups.[3].Value
+            let b = int m.Groups.[4].Value
+            let rest = m.Groups.[5].Value
+            a * b :: (parseMul rest)
+    else
+        []
+and parseNoMul (s:string) =
+    let m = Regex.Match(s, @"do\(\)(.*)")
+    if m.Success then
+        parseMul m.Groups.[1].Value
+    else
+        []
+
+IO.File.ReadLines( fsi.CommandLineArgs.[1] )
+    |> List.ofSeq
+    |> String.concat ""
+    |> parseMul
+    |> List.sum
+    |> printfn "%A"
