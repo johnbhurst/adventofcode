21386df795a42c606288e11824b51533e16f22ab
John Hurst <john.b.hurst@gmail.com>
2024-12-03 18:39:22 +1100

    feat: Added 2024 day3_1 solution.

---
+++ 2024/day3/day3_1.fsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S dotnet fsi
+// Copyright 2024 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2024-12-03
+// See https://adventofcode.com/2024/day/3
+
+open System
+open System.Text.RegularExpressions
+
+let rec parseMul (s:string) =
+    let m = Regex.Match(s, @"mul\(([0-9]{1,3}),([0-9]{1,3})\)(.*)")
+    if m.Success then
+        let a = int m.Groups.[1].Value
+        let b = int m.Groups.[2].Value
+        let rest = m.Groups.[3].Value
+        a * b :: (parseMul rest)
+    else
+        []
+
+IO.File.ReadLines( fsi.CommandLineArgs.[1] )
+    |> Seq.map parseMul
+    |> List.ofSeq
+    |> List.concat
+    |> List.sum
+    |> printfn "%A"
