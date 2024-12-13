b835b5ea5dd5b274e6a29b0b9dfe670245b32c86
John Hurst <john.b.hurst@gmail.com>
2024-12-14 08:56:41 +1300

    docs: Added comments showing steps on test data.

--- 2024/day5/day5_1.fsx
+++ 2024/day5/day5_1.fsx
@@ -59,7 +59,13 @@ let parseUpdate (line:string) =
     line.Split(',') |> List.ofArray |> List.map int
 
 let rules = ruleLines |> List.map parseRule
+// [(47, 53); (97, 13); (97, 61); (97, 47); (75, 29); (61, 13); (75, 53); (29, 13);
+//  (97, 29); (53, 29); (61, 53); (97, 53); (61, 29); (47, 13); (75, 47); (97, 75);
+//  (47, 61); (75, 61); (47, 29); (75, 13); (53, 13)]
+
 let updates = updateLines |> List.map parseUpdate
+// [[75; 47; 61; 53; 29]; [97; 61; 53; 29; 13]; [75; 29; 13]; [75; 97; 47; 61; 53];
+//  [61; 13; 29]; [97; 13; 75; 29; 47]]
 
 let rec ruleOk page rest (page1, page2) =
     if page <> page2 then true // rule doesn't apply
