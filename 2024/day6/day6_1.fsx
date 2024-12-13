a629987b10a3c46bc81fa984591ed28ff239a77e
John Hurst <john.b.hurst@gmail.com>
2024-12-14 11:07:46 +1300

    feat: Added 2024 day6_1 solution.

---
+++ 2024/day6/day6_1.fsx
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S dotnet fsi
+// Copyright 2024 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2024-12-14
+// See https://adventofcode.com/2024/day/6
+
+let input =
+    if fsi.CommandLineArgs.Length > 1
+        then System.IO.File.ReadLines(fsi.CommandLineArgs.[1])
+        else ("""....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...""".Split('\n'))
+
+let grid = array2D input
+
+let rows = Array2D.length1 grid
+let cols = Array2D.length2 grid
+let coords = seq { for i in 0..rows-1 do for j in 0..cols-1 do yield i, j } |> List.ofSeq
+let start = coords |> List.find (fun (i, j) -> grid[i, j] = '^')
+
+let rec moveUp (position: int*int) (positions: Set<int*int>) =
+    let (i, j) = position
+    if i = 0 then positions
+    elif grid[i-1, j] = '#' then moveRight position positions
+    else moveUp (i-1, j) (Set.add (i-1, j) positions)
+and moveRight (position: int*int) (positions: Set<int*int>) =
+    let (i, j) = position
+    if j = cols-1 then positions
+    elif grid[i, j+1] = '#' then moveDown position positions
+    else moveRight (i, j+1) (Set.add (i, j+1) positions)
+and moveDown (position: int*int) (positions: Set<int*int>) =
+    let (i, j) = position
+    if i = rows-1 then positions
+    elif grid[i+1, j] = '#' then moveLeft position positions
+    else moveDown (i+1, j) (Set.add (i+1, j) positions)
+and moveLeft (position: int*int) (positions: Set<int*int>) =
+    let (i, j) = position
+    if j = 0 then positions
+    elif grid[i, j-1] = '#' then moveUp position positions
+    else moveLeft (i, j-1) (Set.add (i, j-1) positions)
+
+moveUp start (Set.singleton start)
+    |> Set.count
+    |> printfn "%A"
