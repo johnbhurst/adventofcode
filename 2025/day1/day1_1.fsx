2d57f4f2afef1cf268f8f5490d59406be127840a
John Hurst <john.b.hurst@gmail.com>
2025-12-05 09:20:40 +1300

    feat: Added 2025 day1_1 solution.

---
+++ 2025/day1/day1_1.fsx
@@ -0,0 +1,32 @@
+#!/usr/bin/env -S dotnet fsi
+// Copyright 2025 John Hurst
+// John Hurst (john.b.hurst@gmail.com)
+// 2025-12-04
+// See https://adventofcode.com/2025/day/1
+
+// L68
+// L30
+// R48
+// L5
+// R60
+// L55
+// L1
+// L99
+// R14
+// L82
+
+let offset (line:string): int =
+    if line.Substring(0,1) = "R"
+        then int(line.Substring(1))
+        else -int(line.Substring(1))
+
+let mod100 n = ((n % 100) + 100) % 100
+
+let newpos current offset = mod100 (current + offset)
+
+System.IO.File.ReadLines(fsi.CommandLineArgs[1])
+    |> Seq.map offset
+    |> Seq.scan newpos 50
+    |> Seq.filter ((=) 0)
+    |> Seq.length
+    |> printfn "%A"
