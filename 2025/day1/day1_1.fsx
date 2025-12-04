9cc4333558aafa6c637fb56f68874b86b5e21471
John Hurst <john.b.hurst@gmail.com>
2025-12-05 09:26:34 +1300

    refactor: Simplifications.

--- 2025/day1/day1_1.fsx
+++ 2025/day1/day1_1.fsx
@@ -16,13 +16,12 @@
 // L82
 
 let offset (line:string): int =
-    if line.Substring(0,1) = "R"
-        then int(line.Substring(1))
-        else -int(line.Substring(1))
+    let sgn = if line.Substring(0,1) = "R" then 1 else -1
+    sgn * int(line.Substring(1))
 
 let mod100 n = ((n % 100) + 100) % 100
 
-let newpos current offset = mod100 (current + offset)
+let newpos current offset = current + offset |> mod100
 
 System.IO.File.ReadLines(fsi.CommandLineArgs[1])
     |> Seq.map offset
